169d286893d2a0f19f627297cc300abcf51f6d08
Walisson Rodrigo <walissonrodrigo@outlook.com>
2020-07-06 03:38:25 -0300

    add action pokemon to use on Redux

---
+++ src/actions/pokemon.js
@@ -0,0 +1,60 @@
+export const POKEMON_SEARCH = "POKEMON_SEARCH";
+export const POKEMON_FETCH = "POKEMON_FETCH";
+export const POKEMON_FETCH_LOADING = "POKEMON_FETCH_LOADING";
+export const POKEMON_FETCH_SUCCESS = "POKEMON_FETCH_SUCCESS";
+export const POKEMON_FETCH_ERROR = "POKEMON_FETCH_ERROR";
+export const CLOSE_SEARCH_DRAWER = "CLOSE_SEARCH_DRAWER";
+export const OPEN_SEARCH_DRAWER = "OPEN_SEARCH_DRAWER";
+export const SEARCHING_POKEMON = "SEARCHING_POKEMON";
+
+export const pokemonSearch = (query) => {
+  return {
+    type: POKEMON_SEARCH,
+    payload: query,
+  };
+};
+
+export const pokemonFetch = () => {
+  return {
+    type: POKEMON_FETCH,
+  };
+};
+
+export const setPokemonFetchLoading = () => {
+  return {
+    type: POKEMON_FETCH_LOADING,
+  };
+};
+
+export const setPokemonFetchSuccess = (data) => {
+  return {
+    type: POKEMON_FETCH_SUCCESS,
+    payload: data,
+  };
+};
+
+export const setPokemonFetchError = (data) => {
+  return {
+    type: POKEMON_FETCH_ERROR,
+    payload: data,
+  };
+};
+
+export const openSearchDrawer = () => {
+  return {
+    type: OPEN_SEARCH_DRAWER,
+  };
+};
+
+export const closeSearchDrawer = () => {
+  return {
+    type: CLOSE_SEARCH_DRAWER,
+  };
+};
+
+export const setSearchingPokemon = (query) => {
+  return {
+    type: SEARCHING_POKEMON,
+    payload: query,
+  };
+};
