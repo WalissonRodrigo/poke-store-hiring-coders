a3de05f2c952e69b70209073ac1ce95f6f02959d
Walisson Rodrigo <walissonrodrigo@outlook.com>
2020-07-06 03:40:27 -0300

    add action cart from use with the Redux

---
+++ src/actions/cart.js
@@ -0,0 +1,53 @@
+export const ADD_POKEMON_CART = "ADD_POKEMON_CART";
+export const REMOVE_POKEMON_CART = "REMOVE_POKEMON_CART";
+export const INCREASE_POKEMON_CART_QTD = "INCREASE_POKEMON_CART_QTD";
+export const DECREASE_POKEMON_CART_QTD = "DECREASE_POKEMON_CART_QTD";
+export const CLEAR_CART = "CLEAR_CART";
+export const CLOSE_CART = "CLOSE_CART";
+export const OPEN_CART = "OPEN_CART";
+
+export const addPokemonCart = (pokemon) => {
+  return {
+    type: ADD_POKEMON_CART,
+    payload: pokemon,
+  };
+};
+
+export const removePokemonCart = (pokemon) => {
+  return {
+    type: REMOVE_POKEMON_CART,
+    payload: pokemon,
+  };
+};
+
+export const increasePokemonCartQtd = (pokemon) => {
+  return {
+    type: INCREASE_POKEMON_CART_QTD,
+    payload: pokemon,
+  };
+};
+
+export const decreasePokemonCartQtd = (pokemon) => {
+  return {
+    type: DECREASE_POKEMON_CART_QTD,
+    payload: pokemon,
+  };
+};
+
+export const clearCart = () => {
+  return {
+    type: CLEAR_CART,
+  };
+};
+
+export const closeCartDrawer = () => {
+  return {
+    type: CLOSE_CART,
+  };
+};
+
+export const openCartDrawer = () => {
+  return {
+    type: OPEN_CART,
+  };
+};
